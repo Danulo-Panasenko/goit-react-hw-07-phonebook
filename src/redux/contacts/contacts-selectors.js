48b2703797e473b42e3ad2a23061461835e00104
Daniil <danielpanasenko777@gmail.com>
2023-03-14 22:05:11 +0200

    main

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -2,6 +2,7 @@ export const getAllContacts = store => store.contacts.items;
 
 export const getFilteredContacts = ({ contacts, filter }) => {
   if (!filter) {
+    // console.log(contacts.items.data);
     return contacts.items;
   }
   const normalizedFilter = filter.toLowerCase();
